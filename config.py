7345dabb69d52e0a73ecafec0097ac12ee4c97c6
Семён Колыванов <gdasdsadasdsa@gmail.com>
2024-11-04 05:44:46 +0500

    postgres db integrate

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/site-constructor'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
