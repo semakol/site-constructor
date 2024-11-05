e0197e520bebb373e4206dbf054079bc1800b325
Evgeni <qazplm2125a@gmail.com>
2024-11-05 19:19:52 +0500

    role add

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(128), nullable=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
