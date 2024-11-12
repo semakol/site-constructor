b73690885d2d958e2e43b3d98995fd08a036628d
Семён Колыванов <gdasdsadasdsa@gmail.com>
2024-11-13 00:32:12 +0500

    auth and register pages

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField
+from wtforms import StringField, EmailField, SubmitField, RadioField
 from wtforms.validators import DataRequired, Email
 import wtforms.validators
 
-class SignInForm(FlaskForm):
+class AuthForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
     password = StringField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Отправить')
+    submit = SubmitField('Войти')
+
+class RegForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    subName = StringField('Фамилия', validators=[DataRequired()])
+    username = StringField('Логин', validators=[DataRequired()])
+    password = StringField('Пароль', validators=[DataRequired()])
+    repPassword = StringField('Повторите Пароль', validators=[DataRequired()])
+    role = RadioField('Роль',
+                      choices=[('editor', ' Я редактор '), ('intern', ' Я стажёр ')],
+                      validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from werkzeug.utils import redirect
 
 from app import app
 from flask import render_template, flash, make_response, session, url_for, request
-from app.forms import SignInForm
+from app.forms import AuthForm, RegForm
 from app.models import *
 
 @app.route('/')
@@ -13,9 +13,9 @@ def index():
     user = User.query.get(session['user_id'])
     return render_template('index.html', session=user.username)
 
-@app.route('/signin', methods=['POST', 'GET'])
-def signin():
-    form = SignInForm()
+@app.route('/auth', methods=['POST', 'GET'])
+def auth():
+    form = AuthForm()
     if request.method == 'POST':
         if form.validate_on_submit():
             username = form.username.data
@@ -24,7 +24,18 @@ def signin():
             user = User.query.filter(User.username == username).first()
             if not user:
                 flash('Неверный логин или пароль')
-                return redirect(url_for('signin'))
+                return redirect(url_for('auth'))
             session['user_id'] = user.id
             return redirect(url_for('index'))
-    return render_template('sign_in.html', form=form)
+    return render_template('auth.html', form=form)
+
+@app.route('/reg', methods=['POST', 'GET'])
+def registration():
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            print(f"Пользователь: {username}, Пароль: {password}")
+            return redirect(url_for('index'))
+    return render_template('registration.html', form=form)
