09aad4644a35335931a78a60db7a38514ae87100
Evgeni <qazplm2125a@gmail.com>
2024-11-23 20:38:15 +0500

    final(no) content

--- app/static/js/content-bar.js
+++ app/static/js/content-bar.js
@@ -1,7 +1,3 @@
-document.querySelector('.head-color').addEventListener('click', function() {
-    this.disabled = false;
-});
-
 function headerClick() {
     // Обновление текстовых блоков
     document.querySelector('.title-0').textContent = document.querySelector('.input-title-0').value;
@@ -25,3 +21,46 @@ function headerClick() {
     document.querySelector('.header-bar').classList.add('hidden');
 }
 
+function infoClick() {
+    // Обновление текстовых блоков
+    document.querySelector('.info-h2').textContent = document.querySelector('.input-info-0').value;
+    document.querySelector('.info-p').textContent = document.querySelector('.input-info-1').value;
+
+    var fileInput = document.querySelector('.input-img-info');
+    var colorInput = document.querySelector('.info-color');
+    var reader = new FileReader();
+    if (colorInput.value && !fileInput.files[0]) 
+        { // Если выбран цвет, удаляем фоновое изображение и устанавливаем цвет 
+            document.querySelector('.info').style.backgroundColor = colorInput.value; 
+            document.querySelector('.info').style.backgroundImage = 'none'; 
+    }else{
+    reader.onload = function(e) {
+        document.querySelector('.info').style.backgroundColor = 'transparent';
+        document.querySelector('.info').style.backgroundImage = `url(${e.target.result})`;
+    }
+    reader.readAsDataURL(fileInput.files[0]);
+    }
+    document.querySelector('.info-bar').classList.add('hidden');
+}
+
+function prospectsClick() {
+    // Обновление текстовых блоков
+    document.querySelector('.prospects-h2').textContent = document.querySelector('.input-prospects-0').value;
+    document.querySelector('.prospects-p').textContent = document.querySelector('.input-prospects-1').value;
+
+    var fileInput = document.querySelector('.input-img-prospects');
+    var colorInput = document.querySelector('.prospects-color');
+    var reader = new FileReader();
+    if (colorInput.value && !fileInput.files[0]) 
+        { // Если выбран цвет, удаляем фоновое изображение и устанавливаем цвет 
+            document.querySelector('.prospects').style.backgroundColor = colorInput.value; 
+            document.querySelector('.prospects').style.backgroundImage = 'none'; 
+    }else{
+    reader.onload = function(e) {
+        document.querySelector('.prospects').style.backgroundColor = 'transparent';
+        document.querySelector('.prospects').style.backgroundImage = `url(${e.target.result})`;
+    }
+    reader.readAsDataURL(fileInput.files[0]);
+    }
+    document.querySelector('.prospects-bar').classList.add('hidden');
+}
