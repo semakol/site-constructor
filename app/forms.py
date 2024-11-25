0094e05ff560c295528febaa8daf1acdd67e8f1e
Семён Колыванов <gdasdsadasdsa@gmail.com>
2024-11-25 20:16:21 +0500

    jinja insert and bd

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, SubmitField, RadioField
+from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired, Email
 import wtforms.validators
 
 class AuthForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
-    password = StringField('Пароль', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
 
 class RegForm(FlaskForm):
     name = StringField('Имя', validators=[DataRequired()])
     subName = StringField('Фамилия', validators=[DataRequired()])
     username = StringField('Логин', validators=[DataRequired()])
-    password = StringField('Пароль', validators=[DataRequired()])
-    repPassword = StringField('Повторите Пароль', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    repPassword = PasswordField('Повторите Пароль', validators=[DataRequired()])
     role = RadioField('Роль',
                       choices=[('editor', ' Я редактор '), ('intern', ' Я стажёр ')],
                       validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,15 @@ from app import app
 from flask import render_template, flash, make_response, session, url_for, request
 from app.forms import AuthForm, RegForm
 from app.models import *
+from app.common import *
 
 @app.route('/')
 def index():
-    user = User.query.get(session['user_id'])
-    return render_template('index.html', session=user.username)
+    user_id = session.get('user_id')
+    user = None
+    if user_id:
+        user = User.query.get(user_id).username
+    return render_template('index.html', session=user)
 
 @app.route('/auth', methods=['POST', 'GET'])
 def auth():
@@ -25,6 +29,9 @@ def auth():
             if not user:
                 flash('Неверный логин или пароль')
                 return redirect(url_for('auth'))
+            if not check_password(password, user.password_hash):
+                flash('Неверный логин или пароль')
+                return redirect(url_for('auth'))
             session['user_id'] = user.id
             return redirect(url_for('index'))
     return render_template('auth.html', form=form)
@@ -36,6 +43,23 @@ def registration():
         if form.validate_on_submit():
             username = form.username.data
             password = form.password.data
+            repPassword = form.repPassword.data
+            role = form.role.data
+            if password != repPassword:
+                flash('Несовпадает пароль')
+                return redirect(url_for('registration'))
             print(f"Пользователь: {username}, Пароль: {password}")
+            new_user = User(username = username, password_hash = hash_password(password), role = role)
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
             return redirect(url_for('index'))
     return render_template('registration.html', form=form)
+
+@app.route('/intership')
+def intership():
+    return render_template('intership.html')
+
+@app.route('/sample')
+def sample():
+    return render_template('sample-0.html')
